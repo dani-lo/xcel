5fde10fb4651df04681d34f5eb3c8c5482741145
dani-lo <resina00@gmail.com>
2021-03-17 09:39:06 +0100

    layout adjustments, footer

--- xcel/frontend/src/pages/account.tsx
+++ xcel/frontend/src/pages/account.tsx
@@ -25,12 +25,12 @@ export const AccountPage = () => {
   if (appStatus === APP_STATUS.LOGGED_IN && userData !== null) {
     return <XContentMain>
       <XPageTitle className="cap">Your Account</XPageTitle>
-      <XSection style={{ marginTop: '4em'}}>
+      <XSection>
         <h3>You are logged in as</h3>
         <p>{ userData.email }</p>
         <UserLogout / >
         </XSection>
-        <XSection style={{ marginTop: '4em'}}>
+        <XSection>
           <h3>Your account data</h3>
           <p>We will need your account details for purchases on this site</p>
         <UserAccount />
@@ -40,12 +40,12 @@ export const AccountPage = () => {
 
   return <XContentMain>
     <XPageTitle className="cap">Your Account</XPageTitle>
-    <XSection style={{ marginTop: '4em'}}> 
+    <XSection> 
       <h2>Register a new account</h2>
       <p style={{ maxWidth: '400px'}}>To be able to buy our prducts you will need to create an account: this requires entering you r email and a password of your choice. You will be then able to add or edit your account detail, which will be used for shipments</p>
       <UserRegistration />
     </XSection>
-    <XSection style={{ marginTop: '4em'}}>
+    <XSection>
       <h2>Or login with your account</h2>
       <p style={{ maxWidth: '400px'}}>Login with your existing username and password</p>
       <UserLogin />
