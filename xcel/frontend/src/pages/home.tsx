49cf0b0e206075c8f7d823e27033214a56990fea
dani <dani@brightanalytics.co.uk>
2021-06-19 20:25:11 +0100

    homepage content again again

--- xcel/frontend/src/pages/home.tsx
+++ xcel/frontend/src/pages/home.tsx
@@ -61,19 +61,19 @@ export const HomePage = () => {
           <div className="flex-row block-responsive-small">
             <StyledMainLogo src="/media/company/xcel-logo.png" className="logo-main" />
             <div>
-              <p className="txt-small padding-bottom">We are a small family run business, dedicated to offering you the same benefits we find daily from out home made range of body creams</p>
+              <p className="txt-small padding-bottom">We are a small family run business, dedicated to offering you the same benefits that we find daily from our home made range of body creams.</p>
               <h3>Our Mission Statement</h3>
-              <p className="txt-small padding-top padding-bottom">We are committed to the highest hygiene standards and quality. We have produce our own body treatments and creams because we love it and believe in it.</p>
-              <p className="txt-small padding-bottom">We sincerely hope that if you try our products, we are sure that you will get as much benefit from them as much as our family does.</p>
+              <p className="txt-small padding-top padding-bottom">We are committed to the highest hygiene standards and quality. We have produced our own body treatments and creams for several years because we love taking care of our own skin and believe in the soothing, regenerating power of natural ingredients.</p>
+              <p className="txt-small padding-bottom">We sincerely hope that you will try our products, and we are sure that you will get as much benefit from them as our family does.</p>
             </div>
           </div>
         </XSection>
       </XContentMain>
       <XSectionHighlight className="fw margin-dub-bottom" style={{ marginTop: 'var(--pad-5)'}}> 
       <h3 className="text-medium">How we started</h3>
-        <p className="txt-small padding-top padding-bottom">We have two beautiful kids: back when our first was born we were looking at body creams, nappy rash creams and all the products you need for their wellbeing</p>
+        <p className="txt-small padding-top padding-bottom">We have two beautiful kids (a boy and a little girl): back when our first was born we were looking at body creams, nappy rash creams and all the products you need for their wellbeing.</p>
         <p className="txt-small padding-top padding-bottom">Sadly,  all we kept seeing were chemicals! Therefore we started studying the use of essential oils, we took workshops and classes and eventually we  started making our own products: the results were incredible! No more nappy rashes, no more skin reactions: so we started using them ourselves with the result that our own skin started to feel much smoother and healthier.</p>
-        <p className="txt-small padding-top padding-bottom">We gave our creams and products to a few friends and the feedback was phenomenal, in fact it was so overwhelmingly good that we decided to start a business and Ixcel Nature was born</p>
+        <p className="txt-small padding-top padding-bottom">We gave our creams and products to a few friends and the feedback was phenomenal, in fact it was so overwhelmingly good that we decided to start a business and Ixcel Nature was born.</p>
       </XSectionHighlight>
     </XContentMain>
     
@@ -121,7 +121,7 @@ export const HomePage = () => {
           <img src="/media/product_images/ingredients/w.jpg" className="margin-half-right fullwidth" style={{ width: '455px', height: '405px' }} />
           <img src="/media/product_images/ingredients/b.png" className="margin-half-left hide-responsive-small fullwidth"  style={{ width: '455px', height: '405px' }} />
         </XSection>
-        <p>The most crucial idea behind our creams is that natural ingredients have the power to help our bodies to get stronger and stimulate them into interaction/reaction rather than passively receive the benefits</p>
+        <p>The most crucial idea behind our creams is that natural ingredients have the power to help our bodies to get stronger and stimulate them into interaction/reaction rather than passively receive the benefits.</p>
         <XButton size="small" onClick={ () => setIngredients(true) } className="margin-dub-top">View All Our Ingredients</XButton>
       </XSection>
     </XContentMain>
